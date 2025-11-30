8695459202d652b5752bcc65b1d783dc19bcb3c1
randoragon <randoragongamedev@gmail.com>
2025-11-30 13:23:18 +0100

    chore: Update flake.lock

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A command-line utility for local notes management";
+ description = "A command-line utility for local notes management";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     secrets.url = "github:randoragon/secrets";
